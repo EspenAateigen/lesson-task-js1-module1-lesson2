694408bf7ba898e2adba67a821707e74d63806e3
EspenAateigen <Espenaateigen@gmail.com>
2021-03-25 15:44:52 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,28 @@ headings4[1].style.backgroundColor = "red";
 const firstP = document.querySelector("p.one");
 firstP.style.fontSize = "24px";
 firstP.style.color = "blue";
+
+// Question 5
+const thirdP = document.querySelector("p.three");
+thirdP.innerHTML = "<span>" + thirdP.innerHTML + "</span>";
+
+// Question 6
+const content = document.querySelector(".content");
+content.innerHTML += "<p>Another paragraph</p>";
+
+// Question 7
+const contentP = document.querySelectorAll(".content p");
+for (let i = 0; i < contentP.length; i++) {
+  contentP[i].classList.add("content-item");
+}
+
+// Question 8
+const services = document.querySelector("#services");
+services.innerHTML = "<li>New list item</li>" + services.innerHTML;
+
+// Question 9
+services.classList.remove("service-list");
+
+// Question 10
+const hide = document.querySelector("#hide");
+hide.style.display = "none";
